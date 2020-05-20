e5af38a18f669e15b0f77dee14f761f8965493ae
faizah1992 <faizah1992@gmail.com>
2020-05-20 10:32:57 -0500

    final

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,50 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  state= {
+    transactions: [],
+    searchTerm: '',
+    filteredTrans: []
+  }
+  
+  componentDidMount(){
+    this.getTransactions()
+  }
+
+  getTransactions = () => {
+    fetch("http://localhost:6001/transactions")
+    .then(resp => resp.json())
+    .then(transactions => (
+      this.setState ({transactions:transactions, filteredTrans:transactions})
+    ))
+  }
+
+  handleSearch = (e) => {
+    this.setState({
+      filteredTrans: this.state.transactions.filter(transaction=>transaction.description.includes(e.target.value))
+    })
+  }
+
+  handleDelete = (deleteTrans) => {
+    fetch (`http://localhost:6001/transactions/${deleteTrans.id}`,{
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    this.setState({
+      transactions: this.state.transactions.filter(transaction => transaction !== deleteTrans),
+      filteredTrans: this.state.transactions.filter(transaction => transaction !== deleteTrans)
+    })
+    {console.log(this.state.filteredTrans)}
+  }
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search search={this.handleSearch}/>
+        <AddTransactionForm getTransactions={this.getTransactions}/>
+        <TransactionsList transactions={this.state.filteredTrans} onDelete={this.handleDelete}/>
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,22 +1,56 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+  state={
+    date: '',
+    description: '',
+    category: '',
+    amount:''
+  }
+
+  setDate = e => this.setState({date: e.target.value})
+
+  setDescription = e => this.setState({description: e.target.value})
+
+  setCategory = e => this.setState({category: e.target.value})
+
+  setAmount = e => this.setState({amount: e.target.value})
+  
+  handleSubmit = (e) => {
+    e.preventDefault()
+    fetch("http://localhost:6001/transactions",{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        date: this.state.date,
+        description: this.state.description,
+        category: this.state.category,
+        amount: this.state.amount
+      })
+    })
+    this.props.getTransactions()
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" value={this.state.date} onChange={this.setDate} />
+            <input type="text" name="description" placeholder="Description" value={this.state.description} onChange={this.setDescription} />
+            <input type="text" name="category" placeholder="Category" value={this.state.category} onChange={this.setCategory}/>
             <input
               type="number"
               name="amount"
               placeholder="Amount"
+              value={this.state.amount} 
+              onChange={this.setAmount}
               step="0.01"
             />
           </div>
-          <button className="ui button" type="submit">
+          <button className="ui button" type="submit" onClick={this.handleSubmit}>
             Add Transaction
           </button>
         </form>
