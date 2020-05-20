6af703f468b3416fbe3a23f8872cdcf9f0f991f6
willfbren <willfbren@gmail.com>
2020-05-20 11:11:20 -0500

    submitted

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,71 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+  state = {
+      transactions: [],
+      searchQuery: ''
+  }
+
+  componentDidMount() {
+      fetch('http://localhost:6001/transactions')
+      .then(resp => resp.json())
+      .then(data => {
+          this.setState({
+              transactions: data
+          })
+      })
+  }
+
+  handleSubmit = (e, newTransaction) => {
+    e.preventDefault()
+    fetch('http://localhost:6001/transactions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(newTransaction)
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        this.setState({
+            transactions: [...this.state.transactions, data]
+        })
+    })
+  }
+
+  handleSearch = (e) => {
+    this.setState({
+        searchQuery: e.target.value
+    })
+  }
+
+  handleDelete = (selection) => {
+      console.log(selection.id)
+      fetch(`http://localhost:6001/transactions/${selection.id}`, {
+          method: 'DELETE'
+      })
+      this.setState({
+        transactions: this.state.transactions.filter(transaction => transaction.id !== selection.id)
+    })
+  }
+  
+  
+  // not working yet
+  handleFilter = (filter) => {
+    console.log(filter)
+  }
+
   render() {
+    let filteredTransactions = this.state.transactions.filter(
+        transaction => transaction.description.toLowerCase().includes(this.state.searchQuery.toLowerCase())
+    )
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search search={this.handleSearch}/>
+        <AddTransactionForm submit={this.handleSubmit}/>
+        <TransactionsList transactions={filteredTransactions} delete={this.handleDelete} filter={this.handleFilter}/>
       </div>
     );
   }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,27 +1,38 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
   return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
-          <th>
-            <h3 className="ui center aligned header">Date</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Description</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Category</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Amount</h3>
-          </th>
-        </tr>
-        {/* render Transactions here */}
-      </tbody>
-    </table>
+    <div>
+      <label>Filter By: </label>
+      <select onChange={(e) => props.filter(e.target.value)}>
+          <option>--</option>
+          <option value='category'>Category</option>
+          <option value='description'>Description</option>
+        </select>
+      <table className="ui celled striped padded table">
+        <tbody>
+          <tr>
+            <th>
+              <h3 className="ui center aligned header">Date</h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">Description</h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">Category</h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">Amount</h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">Delete</h3>
+            </th>
+          </tr>
+          {props.transactions.map(transaction => <Transaction transaction={transaction} key={transaction.id} delete={props.delete}/>)}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
